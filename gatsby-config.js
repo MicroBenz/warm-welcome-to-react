32d5d941f4fe32cf20216a528bc32342a0060db1
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-12-31 17:10:06 +0700

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Warm welcome to React',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-next',
+    'gatsby-plugin-sass'
+  ],
+};
